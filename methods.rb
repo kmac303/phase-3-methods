85356c71bbf1c48b9c51a0109f674786bc472d8a
support <support@kmcintosh-6782.remax.local>
2022-12-31 10:38:03 -0700

    committing

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(param)
+    if param.is_a?(Integer)
+        param / 2
+    else nil
+    end
+end
